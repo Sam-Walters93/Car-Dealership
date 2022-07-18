11fde4f7b8edd255eb43d7dd814a86097ad88b17
Zaw Than <zawlurnthan@gmail.com>
2022-07-18 08:48:12 -0700

    first commit

---
+++ src/main/java/cardealership/models/Address.java
@@ -0,0 +1,10 @@
+package cardealership.models;
+
+public class Address {
+    private int id;
+    private String street1;
+    private String street2;
+    private String city;
+    private String state;
+    public String zipcode;
+}
---
+++ src/main/java/cardealership/models/Vehicle.java
@@ -0,0 +1,144 @@
+package cardealership.models;
+
+import java.math.BigDecimal;
+
+public class Vehicle {
+    private int id;
+    private String make;
+    private String model;
+    private String type;
+    private String bodyStyle;
+    private String year;
+    private String transmission;
+    private String color;
+    private String interior;
+    private int mileage;
+    private String vin;
+    private BigDecimal msrp;
+    private BigDecimal salePrice;
+    private String description;
+    private String picture;
+
+    public Vehicle() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBodyStyle() {
+        return bodyStyle;
+    }
+
+    public void setBodyStyle(String bodyStyle) {
+        this.bodyStyle = bodyStyle;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getInterior() {
+        return interior;
+    }
+
+    public void setInterior(String interior) {
+        this.interior = interior;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public BigDecimal getMsrp() {
+        return msrp;
+    }
+
+    public void setMsrp(BigDecimal msrp) {
+        this.msrp = msrp;
+    }
+
+    public BigDecimal getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(BigDecimal salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
